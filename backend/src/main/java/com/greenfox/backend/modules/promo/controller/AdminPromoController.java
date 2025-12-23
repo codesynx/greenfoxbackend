2c9d8983eba4b479e66e2fbdae0937e56e68de21
Abilmansur <flyxnyfayfryka@gmail.com>
2025-12-23 23:35:50 +0800

    removed incorrect import

--- backend/src/main/java/com/greenfox/backend/modules/promo/controller/AdminPromoController.java
+++ backend/src/main/java/com/greenfox/backend/modules/promo/controller/AdminPromoController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -120,4 +119,3 @@ public class AdminPromoController {
         }
     }
 }
-
